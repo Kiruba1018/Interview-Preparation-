678011fd38d0d4c0693209ea79781d759db0d5ea
Kirubagaran M <146163145+Kiruba1018@users.noreply.github.com>
2024-10-07 13:31:22 +0530

    Add files via upload

---
+++ kmp.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class kmp {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String s1 = s.nextLine();
+        String s2 = s.nextLine();
+        
+    }
+}
