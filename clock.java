f2e3b21e8b40f995f9ce1d9d952e7b88dff1fe33
Kirubagaran M <146163145+Kiruba1018@users.noreply.github.com>
2024-10-09 13:22:34 +0530

    Add files via upload

---
+++ clock.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class clock {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        if(x*y > 12)
+        {
+            int d = x*y -12;
+           
+            if(d>24)
+            System.out.println("-1");
+            else  System.out.println(d);
+        }
+        else{
+            System.out.println(x*y);
+        }
+
+    }
+}
